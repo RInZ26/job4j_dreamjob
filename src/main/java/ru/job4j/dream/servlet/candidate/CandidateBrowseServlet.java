13dba398ebeff6511cb21b257d20690aaa316fdd
RinZ26 <ivs1996@mail.ru>
2021-04-07 18:29:26 +0500

    1. PsqlStore

--- src/main/java/ru/job4j/dream/servlet/candidate/CandidateBrowseServlet.java
+++ src/main/java/ru/job4j/dream/servlet/candidate/CandidateBrowseServlet.java
@@ -1,6 +1,6 @@
 package ru.job4j.dream.servlet.candidate;
 
-import ru.job4j.dream.store.Store;
+import ru.job4j.dream.store.MemStore;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class CandidateBrowseServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("candidates", Store.instOf().findAllCandidates());
+        req.setAttribute("candidates", MemStore.instOf().findAllCandidates());
         req.getRequestDispatcher("candidate/candidates.jsp").forward(req, resp);
     }
 }
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -4,63 +4,45 @@ import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class Store {
+public interface Store {
+    /**
+     * Поиск всех заявок
+     */
+    Collection<Post> findAllPosts();
 
-    private static final Store INST = new Store();
+    /**
+     * Поиск всех кандидатов
+     *
+     * @return
+     */
+    Collection<Candidate> findAllCandidates();
 
-    private static final AtomicInteger POST_ID = new AtomicInteger(4);
+    /**
+     * Сохрание заявки в БД
+     *
+     * @param post сохраняемая заявка
+     */
+    void savePost(Post post);
 
-    private static final AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+    /**
+     * Сохранение кандидата в БД
+     * @param candidate сохраняемый кандидат
+     */
+    void saveCandidate(Candidate candidate);
 
-    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    /**
+     * Поиск заявки по id
+     *
+     * @param id id
+     * @return найденная заявка
+     */
+    Post findPostById(int id);
 
-    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
-
-    private Store() {
-        posts.put(1, new Post(1, "Junior Java Job"));
-        posts.put(2, new Post(2, "Middle Java Job"));
-        posts.put(3, new Post(3, "Senior Java Job"));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
-    }
-
-    public static Store instOf() {
-        return INST;
-    }
-
-    public Collection<Post> findAllPosts() {
-        return posts.values();
-    }
-
-    public Collection<Candidate> findAllCandidates() {
-        return candidates.values();
-    }
-
-    public void savePost(Post post) {
-        if (post.getId() == 0) {
-            post.setId(POST_ID.incrementAndGet());
-        }
-        posts.put(post.getId(), post);
-    }
-
-    public void saveCandidate(Candidate candidate) {
-        if(candidate.getId() == 0) {
-            candidate.setId(CANDIDATE_ID.incrementAndGet());
-        }
-        candidates.put(candidate.getId(), candidate);
-    }
-
-    public Post findPostById(int id) {
-        return posts.get(id);
-    }
-
-    public Candidate finCandidateById(int id) {
-        return candidates.get(id);
-    }
+    /**
+     * Поиск кандидата по id
+     * @param id id
+     * @return найденный кандидат
+     */
+    Candidate finCandidateById(int id);
 }
